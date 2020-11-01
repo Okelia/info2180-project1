609cd2eff1ab8cc229af2a4567d83c93c8f8b485
Okelia Green <greenokelia@gmail.com>
2020-11-01 13:09:04 -0500

    Add form

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", ()=> {
-    
+    let e = document.getElementsByClassName("btn")
+    let enterdata =  document.getElementById("email")
+      
+    e.addEventListener("click", emailFun(enterdata))
+
+    function emailFun(data){
+        let mail = data.email.value;
+        var messageElement = document.querySelector('.message');
+        if (mail){
+            messageElement.innerHTML = "Thank you! Your email address <${mail}> has been added to your mailing list";
+            return false;
+        }
+        else{ 
+            messageElement.innerHTML = "Please enter a valid email address ";
+            return false;
+        }
+    }
 })
